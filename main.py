568983a3c0078c2b93f75abdaa8a2f9e6ba9e045
Kilari Bindu Yadav <binduyadav.kilari@gmail.com>
2022-08-26 18:13:04 +0530

    CAL-12 Created division function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import addition # contains add function that returns sum of two numbers
 import mod # contains mod function
-first = 100
+import division # contains division function
+first = 99
 second = 63
 
 print(f"{first} + {second} = {addition.add(first,second)}")
 print(f"{first} % {second} = {mod.mod(first,second)}")
+print(f"{first} / {second} = {division.division(first,second)}")
