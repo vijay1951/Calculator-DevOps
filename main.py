c9ed11d5c25dcb61ff9dd886b388a30a409b39ec
vijay1951 <60980946+vijay1951@users.noreply.github.com>
2022-08-26 17:54:04 +0530

    Update main.py
    added multiplication code on main method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import addition # contains add function that returns sum of two numbers
 import mod # contains mod function
+import Multiplication
 first = 100
 second = 63
 
 print(f"{first} + {second} = {addition.add(first,second)}")
 print(f"{first} % {second} = {mod.mod(first,second)}")
+print(f"{first} * {second} = {Multiplication.Multiplication(first,second)}")
